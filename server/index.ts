22147fea5c0add3140c591926d8b10773ea056db
YEO Yédjandé <yeoyedjande@gmail.com>
2025-08-21 19:01:25 +0000

    Retouche le fichier index

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,12 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    //serveStatic(app);
+    const fs = require('fs');
+    if (fs.existsSync('./server/public')) {
+      serveStatic(app);
+    }
+    
   }
 
   // ALWAYS serve the app on port 5000
