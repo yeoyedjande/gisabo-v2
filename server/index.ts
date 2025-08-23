1ad3d2bb74ad8d2b6ea7b6b0264647ccfb6a00d1
YEO Yédjandé <yeoyedjande@gmail.com>
2025-08-23 23:58:00 +0000

    Changement sur le port 3000

--- server/index.ts
+++ server/index.ts
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 8080;
+  const port = 3000;
   server.listen({
     port,
     host: "0.0.0.0",
